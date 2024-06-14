a7ca614acf1d2993b29d1ad9709a6ea353291db8
marsel-b-games <marselbkak@mail.ru>
2024-06-14 22:51:15 +0500

    Fix Some Bugs

--- App_MVP/Assets/Core/Modules/_Core/Parametrs/Scenes/SceneParametrsSettings.cs
+++ App_MVP/Assets/Core/Modules/_Core/Parametrs/Scenes/SceneParametrsSettings.cs
@@ -2,41 +2,21 @@ using UnityEngine;
 
 namespace Core.Modules.Core.Parametrs
 {
-#if UNITY_EDITOR
-    using UnityEditor;
-#endif
-
     [CreateAssetMenu(fileName = "Scene Settings", menuName = "Core/Parametrs/SceneSettings")]
     public class SceneParametrsSettings : ScriptableObject
     {
-#if UNITY_EDITOR
-        [SerializeField] private SceneAsset _mainScene;
-        [SerializeField] private SceneAsset _startScene;
-#endif
+        [SerializeField] private string _mainScene;
+        [SerializeField] private string _startScene;
 
         public string MainScene
         {
-            get
-            {
-#if UNITY_EDITOR
-                return _mainScene != null ? _mainScene.name : string.Empty;
-#else
-                return string.Empty;
-#endif
-            }
+            get { return _mainScene; }
             private set { }
         }
 
         public string StartScene
         {
-            get
-            {
-#if UNITY_EDITOR
-                return _startScene != null ? _startScene.name : string.Empty;
-#else
-                return string.Empty;
-#endif
-            }
+            get { return _startScene; }
             private set { }
         }
     }
